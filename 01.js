ba17aad70f03ecec628d8e8151a229a796180df6
byakuyafox1 <119981967+byakuyafox1@users.noreply.github.com>
2023-02-10 23:49:32 -0300

    01 terminado

--- 01.js
+++ 01.js
@@ -11,6 +11,13 @@ function soloNumeros(array) {
   // soloNumeros([1, 'Henry', 2]) debe retornar [1, 2]
 
   // Tu código aca:
+  let arreglo = [];
+  for (let i = 0; i < array.length; i++) {
+   if (typeof(array[i]) === "number") {
+      arreglo.push(array[i])
+   }
+  }
+  return arreglo;
 
 }
 
